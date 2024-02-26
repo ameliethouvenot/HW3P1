0ac355b1c5d2d43c03bdd7cf2d15e2ef4901ab79
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-02-26 19:45:31 +0000

    HW3P2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,19 @@
 class EntriesController < ApplicationController
+
+def new 
+@entry = Entry.new 
+end 
+
+def create 
+
+  @entry = Entry.new
+  @entry["title"] = params["title"]
+  @entry["description"] = params["description"]
+  @entry["posted_on"] = params["posted_on"]
+  @entry["place_id"] = params["place_id"]
+  @entry.save
+  redirect_to "/places#{@entry["place_id"]}"
+end 
+
+
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@ class PlacesController < ApplicationController
 
   def show
   @place = Place.find_by({"id"=> params["id"]})  
-  #@entries = Entry.where({"place_id" => @places["id"]})
+  @entries = Entry.where({"place_id" => @places["id"]})
   end
 
   def new 
