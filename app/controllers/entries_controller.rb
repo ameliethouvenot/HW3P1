fe79f96f89695f3375986758dca0fb63bffb1b8f
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-02-28 08:04:57 +0000

    FINAL CHANGES

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,14 @@
 class EntriesController < ApplicationController
 
-def show 
-  @entry = Entry.find_by({"id" => params["id"]})
-  @place = Place.find_by({"id" => @entry["place_id"]})
-end 
+#def show 
+#  @entry = Entry.find_by({"id" => params["id"]})
+#  @place = Place.find_by({"id" => @entry["place_id"]})
+#end 
 
 def new 
-#@place = Place.find_by({ "id" => params["place_id"] })
-@entry = Entry.new 
-@entry.place_id = params["place_id"]
+#@entry = Entry.new 
+@place = Place.find_by({ "id" => params["place_id"] })
+#@entry.place_id = params["place_id"]
 end 
 
 def create 
@@ -18,7 +18,7 @@ def create
   @entry["posted_on"] = params["posted_on"]
   @entry["place_id"] = params["place_id"]
   @entry.save
-  redirect_to "/places#{@entry["place_id"]}"
+  redirect_to "/places/#{@entry["place_id"]}"
 end 
 
 
